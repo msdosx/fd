b98f8f0387c6823a692716ec40db049e2dc7b53d
msdos <1msdos1@gmail.com>
2013-10-12 13:06:20 +0400

    add user enable token. change nickname to username

--- db/migrate/20131012085341_create_users.rb
+++ db/migrate/20131012085341_create_users.rb
@@ -1,14 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :nickname
+      t.string :username, :null => false, :default => ""
       t.string :name
       t.string :soname
       t.string :lastname
       t.date :bday
       t.text :hobby
+      t.boolean :enabled, :default => true
 
       t.timestamps
     end
+    add_index :users, :email,                :unique => true
   end
 end
