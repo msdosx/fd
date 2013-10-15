c0c34748f8cbaa8041a8da3d22f4d2ceae79b451
msdos <1msdos1@gmail.com>
2013-10-15 23:41:01 +0400

    add init data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Role.create ([{name: 'user'}, {name: 'admin'}])
+Role.create [{name: 'user'}, {name: 'admin'}]
 User.create username: 'admin', name: 'Yuri', soname: 'Panchenko',email: '1msdos1@gmail.com',
             password: '123123123', password_confirmation: '123123123'
-User.create username: 'user', name: 'Yuri', soname: 'Panchenko',email: '11msdos1@gmail.com',
-            password: '123123123', password_confirmation: '123123123'
+
 200.times do |ind|
   User.create username: "user#{ind}", name: 'Yuri', soname: 'Panchenko',email: "#{ind}msdos1@gmail.com",
               password: '123123123', password_confirmation: '123123123'
 end
 
-            #encrypted_password: '$2a$10$4jP1sXBhdS3JHl5aZVd7/uzhaEGiVedNc1H.PrweF8yMal7/e/GpG'
 @admin = User.find_by_username('admin')
 @admin.roles << Role.find_by_name(:admin)
 @admin.save
-Tag.create name: 'hitech'
+Tag.create [{name: 'hitech'}, {name: 'travel'}, {name: 'cars'}, {name: 'fashion'}, {name: 'politics'}]
+
+Opinion.create nickname: "appstore", about: "apple store as is", tag_id: Tag.find_by_name('hitech').id
 puts "database init done"
