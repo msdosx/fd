a8ac352e7fae5e87bdd14150769fe5e958495bc9
msdos <1msdos1@gmail.com>
2013-10-23 15:59:30 +0400

    small changes in gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fd::Application.config.secret_key_base = '50e9351edb36f4dbe22d236bdbc03d594d4552779c90b8eb22821a9525e8f51143ea49185ebd476111a4afa0d68c89b7b80da91ecd9fa3901bc507ef6d5cd48d'
